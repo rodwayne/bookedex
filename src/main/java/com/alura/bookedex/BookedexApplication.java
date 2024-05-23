d081c436d32025f2b8e64421b26e8c7d5f88a633
rodwayne <ricardo260898@gmail.com>
2024-05-22 21:10:49 -0500

    First commit

---
+++ src/main/java/com/alura/bookedex/BookedexApplication.java
@@ -0,0 +1,13 @@
+package com.alura.bookedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookedexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/bookedex/BookedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.bookedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
