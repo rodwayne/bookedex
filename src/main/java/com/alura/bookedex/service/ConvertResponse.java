a150b3b1ee258e9e2d935b68e342e16a0f41214a
rodwayne <ricardo260898@gmail.com>
2024-05-22 21:36:18 -0500

    Create ConvertResponse class

---
+++ src/main/java/com/alura/bookedex/service/ConvertResponse.java
@@ -0,0 +1,17 @@
+package com.alura.bookedex.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConvertResponse implements IConvertResponse {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T fetch(String json, Class<T> genericClass) {
+        try {
+            return objectMapper.readValue(json, genericClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
