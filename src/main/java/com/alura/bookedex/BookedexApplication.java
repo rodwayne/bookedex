1762f44cb920f984a544b300c1c6fef54e44b305
rodwayne <ricardo260898@gmail.com>
2024-05-22 21:57:07 -0500

    Overrides run method

--- src/main/java/com/alura/bookedex/BookedexApplication.java
+++ src/main/java/com/alura/bookedex/BookedexApplication.java
@@ -1,13 +1,22 @@
 package com.alura.bookedex;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alura.bookedex.main.Main;
+
 @SpringBootApplication
-public class BookedexApplication {
+public class BookedexApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookedexApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.menu();
 	}
 
 }
