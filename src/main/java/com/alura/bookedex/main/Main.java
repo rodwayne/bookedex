49ea03e026d7d6a00e66ce8fa645f805f5576628
rodwayne <ricardo260898@gmail.com>
2024-05-31 14:28:29 -0500

    Search book by name

--- src/main/java/com/alura/bookedex/main/Main.java
+++ src/main/java/com/alura/bookedex/main/Main.java
@@ -1,6 +1,8 @@
 package com.alura.bookedex.main;
 
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.Scanner;
 
 import com.alura.bookedex.model.BookData;
 import com.alura.bookedex.model.ResponseData;
@@ -12,6 +14,7 @@ public class Main {
     private static final String BASE_URL = "https://gutendex.com/books/";
     private Api api = new Api();
     private ConvertResponse convertResponse = new ConvertResponse();
+    private Scanner input = new Scanner(System.in);
 
     public void menu() {
         var json = api.fetch(BASE_URL);
@@ -26,5 +29,21 @@ public class Main {
         .limit(10)
         .map(book -> book.title().toUpperCase())
         .forEach(System.out::println);
+
+        // Search book by name
+        System.out.println("Enter a book to search: ");
+        var bookName = input.nextLine();
+        var jsonBookName = api.fetch(BASE_URL + "?search=" + bookName.replace(" ", "+"));
+        var responseBookName = convertResponse.fetch(jsonBookName, ResponseData.class);
+        Optional<BookData> bookNameOptional = responseBookName.booksResults().stream()
+        .filter(book -> book.title().toUpperCase().contains(bookName.toUpperCase()))
+        .findFirst();
+
+        if (bookNameOptional.isPresent()) {
+            System.out.println("Book found");
+            System.out.println(bookNameOptional.get());
+        } else {
+            System.out.println(bookName + " not found");
+        }
     }
 }
